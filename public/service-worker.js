32e34fddf107d99a6bc5dff350950aca6740958f
Matt Weichel <maweiche@gmail.com>
2021-11-18 14:13:00 -0800

    fixed service worker, proper one loading

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,14 @@
-//Global Constants
 const APP_PREFIX = 'Budget-Tracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
-
-//Array of files to Cache
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/manifest.json',
-    '/css/style.css',
-    '/js/index.js',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
-]
+    './index.html',
+    './manifest.json',
+    './style.css',
+    './index.js',
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png'
+];
 
 self.addEventListener('install', function (e) {
     e.waitUntil(
